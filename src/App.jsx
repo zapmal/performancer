4cfcf2a0fc56f34a558b6d8f1aea07d2de5c5187
Zondazx <mzxgd16@gmail.com>
2020-12-22 14:24:53 -0400

    feat: finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 
-import "./App.css";
+import './App.css';
+
+const METRICS = ['TTFB', 'LCP', 'FID', 'FCP', 'CLS'];
 
 const App = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    chrome.runtime.sendMessage({
+      type: 'performance:metric:request',
+    }, (data) => {
+      setData(data);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Page Metric</h1>
+      <table>
+        <thead>
+          <tr>
+            <th width='20%'></th>
+            {METRICS.map(metric => (
+              <th key={metric} width='5%'>{metric}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(data).map((url) => (
+            <tr key={url}>
+              <td>{url.slice(0, 30)}</td>
+              {METRICS.map((metric) => (
+                <td key={[url, metric].join('')} width='10%' style={{ textAlign: 'center' }}>
+                  {Math.round((data[url][metric] || { average: 0 }).average)}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,1 +1,51 @@
-console.log("contentScript.js");
+import { 
+  getTTFB,
+  getLCP,
+  getFID,
+  getFCP,
+  getCLS
+} from 'web-vitals';
+
+const informationDiv = document.createElement('div');
+
+informationDiv.style.position = 'fixed';
+informationDiv.style.left = 0;
+informationDiv.style.top = 0;
+informationDiv.style.zIndex = 0;
+informationDiv.style.backgroundColor = 'black';
+informationDiv.style.color = 'white';
+informationDiv.style.padding = '1rem';
+informationDiv.style.fontFamily = 'Segoe UI';
+
+document.body.appendChild(informationDiv);
+
+const metrics = {};
+
+const gatherMetrics = ({ name, value }) => {
+  metrics[name] = value;
+
+  chrome.runtime.sendMessage({
+    type: 'performance:metric',
+    name,
+    value,
+  });
+
+  const metricsHTML = Object.keys(metrics)
+    .map((key) => `<div>${key}</div><div>${Math.round(metrics[key])}</div>`)
+    .join('');
+
+  informationDiv.innerHTML=`
+    <div style='font-weight: bold; font-size: x-large;'>Performance Metrics</div>
+    <div style='display:grid; grid-template-columns: 1fr 1fr; grid-column-gap: 1rem;'>
+       <div>Metric</div> 
+       <div>Value</div>
+       ${metricsHTML}
+    </div>
+  `;
+};
+
+getTTFB(gatherMetrics);
+getLCP(gatherMetrics);
+getFID(gatherMetrics);
+getFCP(gatherMetrics);
+getCLS(gatherMetrics);
